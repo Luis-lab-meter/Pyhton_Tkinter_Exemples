8eb79a33e579024cdfbc9113a9a666ad4425b4be
Luis-lab-meter <56420023+Luis-lab-meter@users.noreply.github.com>
2021-05-29 22:58:58 -0400

    Update child.py

--- child.py
+++ child.py
@@ -3,19 +3,19 @@ import tkinter as tk
 class WindowSub(object):
     def __init__(self, parent):
         self.toplevel = tk.Toplevel(parent)
+
         self.var = tk.StringVar()
         label = tk.Label(self.toplevel, text="Pick something:")
-        om = tk.OptionMenu(self.toplevel, self.var, "one", "two","three", command=self.getValue)
+        om = tk.OptionMenu(self.toplevel, self.var, "one", "two","three", command=self.destroyWindow)
         label.pack(side="top", fill="x")
         om.pack(side="top", fill="x")
 
-    def makeSometing(self, event):
-        self.toplevel.deiconify()
+    def waitAndGetValue(self, event):
         self.toplevel.wait_window()
         value = self.var.get()
         return value
 
-    def getValue(self, event):
+    def destroyWindow(self, event):
         self.toplevel.destroy()
 
 if __name__ == "__main__":
